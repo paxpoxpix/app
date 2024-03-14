b63275200547cc9f20f184d49ad3e3e8fe496ce6
Pizza <130300211+paxpoxpix@users.noreply.github.com>
2024-03-14 16:49:18 +0000

    Update RegisterPage.tsx

--- client/pages/RegisterPage.tsx
+++ client/pages/RegisterPage.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 import { register } from '../features/auth/authSlice';
 
 const RegisterPage: React.FC = () => {
