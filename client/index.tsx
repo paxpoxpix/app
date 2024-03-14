73122f9a954c60bce3821434493fb5500482567a
Pizza <130300211+paxpoxpix@users.noreply.github.com>
2024-03-14 16:51:27 +0000

    Update index.tsx

--- client/index.tsx
+++ client/index.tsx
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './features/auth/authSlice'
-import { reducer as characterReducer } from './features/characters/charactersSlice';
-import { reducer as conversationReducer } from './features/conversations/conversationsSlice';
+import { characterReducer } from './features/characters/charactersSlice';
+import { conversationReducer } from './features/conversations/conversationsSlice';
 import App from './App';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
