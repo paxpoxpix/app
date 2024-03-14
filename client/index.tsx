acc41507762c483a4fe88e206c6312a9248b4496
Pizza <130300211+paxpoxpix@users.noreply.github.com>
2024-03-14 16:49:59 +0000

    Update index.tsx

--- client/index.tsx
+++ client/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { reducer as authReducer } from './features/auth/authSlice';
+import { authReducer } from './features/auth/authSlice'
 import { reducer as characterReducer } from './features/characters/charactersSlice';
 import { reducer as conversationReducer } from './features/conversations/conversationsSlice';
 import App from './App';
